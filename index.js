8298510d45471e9f8b22f044a313474b4d3cbbde
Nicklye26 <nicklye26@gmail.com>
2023-01-30 22:33:00 +0800

    Unexpected app crashed, need to review Index.js

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const cors = require('cors')
-const express = require('express')
-require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+const checkJwt = auth({
+  audience: process.env.AUTH_AUDIENCE,
+  issuerBaseURL: process.env.AUTH_ISSUER_BASE_URL,
+});
+require("dotenv").config();
 
 // importing Routers
-const ListingsRouter = require('./routers/listingsRouter')
+const ListingsRouter = require("./routers/listingsRouter");
 
 // importing Controllers
-const ListingsController = require('./controllers/listingsController')
+const ListingsController = require("./controllers/listingsController");
 
 // importing DB
-const db = require('./db/models/index')
-const { listing, user } = db;
+const db = require("./db/models/index");
+const { Listing, User } = db;
 
 // initializing Controllers -> note the lowercase for the first word
-const listingsController = new ListingsController(listing, user)
+const listingsController = new ListingsController(Listing, User);
 
 // inittializing Routers
-const listingsRouter = new ListingsRouter(listingsController).routes()
+const listingsRouter = new ListingsRouter(
+  listingsController,
+  checkJwt
+).routes();
 
 const PORT = process.env.PORT;
 const app = express();
@@ -28,7 +36,10 @@ app.use(cors());
 app.use(express.json());
 
 // enable and use router
-app.use('/listings', listingsRouter)
+app.use("/listings", listingsRouter);
+
+// enable auth from auth0
+app.use(auth());
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
