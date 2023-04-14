8a7d21d995cd957831ea37690eb6261f676e09a1
ivan89kiat <ivan89kiat@gmail.com>
2023-04-14 09:07:11 +0700

    auth0 implementation

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -8,11 +8,14 @@ class ListingsController extends BaseController {
 
   /** if a method in this extended class AND the base class has the same name, the one in the extended class will run over the base method */
   // Create listing. Requires authentication.
-  async insertOne(req, res) {
+  insertOne = async (req, res) => {
     const { title, category, condition, price, description, shippingDetails } =
       req.body;
+
     try {
-      // TODO: Get seller email from auth, query Users table for seller ID
+      const [seller] = await this.userModel.findOrCreate({
+        where: { email: req.body.sellerEmail },
+      });
 
       // Create new listing
       const newListing = await this.model.create({
@@ -23,18 +26,19 @@ class ListingsController extends BaseController {
         description: description,
         shippingDetails: shippingDetails,
         buyerId: null,
-        sellerId: 1, // TODO: Replace with seller ID of authenticated seller
+        sellerId: seller.id,
       });
 
       // Respond with new listing
       return res.json(newListing);
     } catch (err) {
+      console.log(err);
       return res.status(400).json({ error: true, msg: err });
     }
-  }
+  };
 
   // Retrieve specific listing. No authentication required.
-  async getOne(req, res) {
+  getOne = async (req, res) => {
     const { listingId } = req.params;
     try {
       const output = await this.model.findByPk(listingId);
@@ -42,23 +46,27 @@ class ListingsController extends BaseController {
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
-  }
+  };
 
   // Buy specific listing. Requires authentication.
-  async buyItem(req, res) {
+  buyItem = async (req, res) => {
     const { listingId } = req.params;
+
     try {
       const data = await this.model.findByPk(listingId);
 
-      // TODO: Get buyer email from auth, query Users table for buyer ID
-      await data.update({ BuyerId: 1 }); // TODO: Replace with buyer ID of authenticated buyer
+      const [buyer] = await this.userModel.findOrCreate({
+        where: { email: req.body.buyerEmail },
+      });
+
+      await data.update({ BuyerId: buyer.id });
 
       // Respond to acknowledge update
       return res.json(data);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
-  }
+  };
 }
 
 module.exports = ListingsController;
--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-const cors = require('cors')
-const express = require('express')
-require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+const { auth } = require("express-oauth2-jwt-bearer");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const checkJwt = auth({
+  audience: "https://carousell/api",
+  issuerBaseURL: "https://dev-snbko3iqzlm4bomi.us.auth0.com/",
+});
 
 // importing Routers
-const ListingsRouter = require('./routers/listingsRouter')
+const ListingsRouter = require("./routers/listingsRouter");
 
 // importing Controllers
-const ListingsController = require('./controllers/listingsController')
+const ListingsController = require("./controllers/listingsController");
 
 // importing DB
-const db = require('./db/models/index')
+const db = require("./db/models/index");
 const { listing, user } = db;
 
 // initializing Controllers -> note the lowercase for the first word
-const listingsController = new ListingsController(listing, user)
+const listingsController = new ListingsController(listing, user);
 
 // inittializing Routers
-const listingsRouter = new ListingsRouter(listingsController).routes()
+const listingsRouter = new ListingsRouter(
+  listingsController,
+  checkJwt
+).routes();
 
 const PORT = process.env.PORT;
-const app = express();
-
-// Enable CORS access to this server
-app.use(cors());
-
-// Enable reading JSON request bodies
-app.use(express.json());
 
 // enable and use router
-app.use('/listings', listingsRouter)
+app.use("/listings", listingsRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
