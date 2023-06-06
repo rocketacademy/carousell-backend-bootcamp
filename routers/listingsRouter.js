84c34c72f0f12cf2d92e3d5f5b3e899fda19ccae
hengmhs <hengmhs@gmail.com>
2023-06-07 04:49:41 +0800

    Added basic boilerplate for authentication

--- routers/listingsRouter.js
+++ routers/listingsRouter.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
+const { auth } = require("express-oauth2-jwt-bearer");
+const checkJwt = auth();
 class ListingsRouter {
-    constructor(controller){
-        this.controller = controller
-    }
-    routes(){
-        // we will insert routes into here later on
-        router.get('/', this.controller.getAll.bind(this.controller))
-        router.post('/', this.controller.insertOne.bind(this.controller))
-        router.get('/:listingId', this.controller.getOne.bind(this.controller))
-        router.put('/:listingId', this.controller.buyItem.bind(this.controller))
-        return router
-    }
+  constructor(controller) {
+    this.controller = controller;
+  }
+  routes() {
+    // we will insert routes into here later on
+    router.get("/", this.controller.getAll.bind(this.controller));
+    router.post("/", checkJwt, this.controller.insertOne.bind(this.controller));
+    router.get("/:listingId", this.controller.getOne.bind(this.controller));
+    router.put(
+      "/:listingId",
+      checkJwt,
+      this.controller.buyItem.bind(this.controller)
+    );
+    return router;
+  }
 }
 
-module.exports = ListingsRouter
+module.exports = ListingsRouter;
