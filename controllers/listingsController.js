37b31ef4319c1829ec31b61fe50ae440f449faeb
JerryTohHS <jerrytoh93@gmail.com>
2023-08-21 22:05:57 +0800

    Backend

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -1,3 +1,4 @@
+const { defaults } = require("pg");
 const BaseController = require("./baseController");
 
 class ListingsController extends BaseController {
@@ -9,10 +10,31 @@ class ListingsController extends BaseController {
   /** if a method in this extended class AND the base class has the same name, the one in the extended class will run over the base method */
   // Create listing. Requires authentication.
   async insertOne(req, res) {
-    const { title, category, condition, price, description, shippingDetails } =
-      req.body;
+    const {
+      title,
+      category,
+      condition,
+      price,
+      description,
+      shippingDetails,
+      sellerEmail,
+      sellerFirstName,
+      sellerLastName,
+      sellerPhone,
+    } = req.body;
     try {
+      // get authenticated user's email from request
+      const authenticatedEmail = sellerEmail;
       // TODO: Get seller email from auth, query Users table for seller ID
+      const [user, created] = await this.userModel.findOrCreate({
+        where: { email: authenticatedEmail },
+        defaults: {
+          firstName: sellerFirstName,
+          lastName: sellerLastName,
+          phoneNum: sellerPhone,
+          email: sellerEmail,
+        },
+      });
 
       // Create new listing
       const newListing = await this.model.create({
@@ -23,7 +45,7 @@ class ListingsController extends BaseController {
         description: description,
         shippingDetails: shippingDetails,
         buyerId: null,
-        sellerId: 1, // TODO: Replace with seller ID of authenticated seller
+        sellerId: user.id, // TODO: Replace with seller ID of authenticated seller = Retrieved user's ID
       });
 
       // Respond with new listing
@@ -47,11 +69,23 @@ class ListingsController extends BaseController {
   // Buy specific listing. Requires authentication.
   async buyItem(req, res) {
     const { listingId } = req.params;
+    const { buyerEmail, buyerFirstName, buyerLastName, buyerPhone } = req.body;
     try {
+      // get authenticated user's email from request
+      const authenticatedEmail = buyerEmail;
       const data = await this.model.findByPk(listingId);
 
       // TODO: Get buyer email from auth, query Users table for buyer ID
-      await data.update({ BuyerId: 1 }); // TODO: Replace with buyer ID of authenticated buyer
+      const [user, created] = await this.userModel.findOrCreate({
+        where: { email: authenticatedEmail },
+        defaults: {
+          firstName: buyerFirstName,
+          lastName: buyerLastName,
+          phoneNum: buyerPhone,
+          email: buyerEmail,
+        },
+      });
+      await data.update({ buyerId: user.id }); // TODO: Replace with buyer ID of authenticated buyer
 
       // Respond to acknowledge update
       return res.json(data);
