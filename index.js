5cfc2146c54b0ceb25373c07ced318614515f083
ianlauyin <ianlauyin@gmail.com>
2023-12-28 21:25:37 +0800

    Update with auth0

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const cors = require('cors')
-const express = require('express')
-require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+require("dotenv").config();
 
 // importing Routers
-const ListingsRouter = require('./routers/listingsRouter')
+const ListingsRouter = require("./routers/listingsRouter");
 
 // importing Controllers
-const ListingsController = require('./controllers/listingsController')
+const ListingsController = require("./controllers/listingsController");
 
 // importing DB
-const db = require('./db/models/index')
+const db = require("./db/models/index");
 const { listing, user } = db;
-
-// initializing Controllers -> note the lowercase for the first word
-const listingsController = new ListingsController(listing, user)
-
-// inittializing Routers
-const listingsRouter = new ListingsRouter(listingsController).routes()
 
 const PORT = process.env.PORT;
 const app = express();
+const checkJwt = auth({
+  audience: process.env.AUDIENCE,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+});
+app.use(auth());
+// initializing Controllers -> note the lowercase for the first word
+const listingsController = new ListingsController(listing, user);
+
+// inittializing Routers
+const listingsRouter = new ListingsRouter(
+  listingsController,
+  checkJwt
+).routes();
 
 // Enable CORS access to this server
 app.use(cors());
@@ -28,7 +36,7 @@ app.use(cors());
 app.use(express.json());
 
 // enable and use router
-app.use('/listings', listingsRouter)
+app.use("/listings", listingsRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
