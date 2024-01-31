ac163c00af559cc26926545a5e65b95ea37e7197
estney <esther.pxy@gmail.com>
2024-01-31 17:47:30 +0800

    can sell

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -9,10 +9,23 @@ class ListingsController extends BaseController {
   /** if a method in this extended class AND the base class has the same name, the one in the extended class will run over the base method */
   // Create listing. Requires authentication.
   async insertOne(req, res) {
-    const { title, category, condition, price, description, shippingDetails } =
-      req.body;
+    const {
+      title,
+      category,
+      condition,
+      price,
+      description,
+      shippingDetails,
+      email,
+    } = req.body;
     try {
       // TODO: Get seller email from auth, query Users table for seller ID
+
+      const [seller] = await this.userModel.findOrCreate({
+        where: {
+          email: email,
+        },
+      });
 
       // Create new listing
       const newListing = await this.model.create({
@@ -23,7 +36,7 @@ class ListingsController extends BaseController {
         description: description,
         shippingDetails: shippingDetails,
         buyerId: null,
-        sellerId: 1, // TODO: Replace with seller ID of authenticated seller
+        sellerId: seller.id,
       });
 
       // Respond with new listing
@@ -51,7 +64,12 @@ class ListingsController extends BaseController {
       const data = await this.model.findByPk(listingId);
 
       // TODO: Get buyer email from auth, query Users table for buyer ID
-      await data.update({ BuyerId: 1 }); // TODO: Replace with buyer ID of authenticated buyer
+      const buyer = await this.userModel.findOrCreate({
+        where: {
+          email: req.body.buyerEmail,
+        },
+      });
+      await data.update({ BuyerId: buyer.email }); // TODO: Replace with buyer ID of authenticated buyer
 
       // Respond to acknowledge update
       return res.json(data);
