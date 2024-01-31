e98a682385d723671fb2b06e1d6e1146bf477ab3
Chxshire22 <akuindra22@gmail.com>
2024-01-31 13:51:11 +0800

    added jwtAuth

--- routers/listingsRouter.js
+++ routers/listingsRouter.js
@@ -2,15 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 class ListingsRouter {
-    constructor(controller){
+    constructor(jwtCheck,controller){
+				this.jwtCheck = jwtCheck
         this.controller = controller
     }
     routes(){
         // we will insert routes into here later on
         router.get('/', this.controller.getAll.bind(this.controller))
-        router.post('/', this.controller.insertOne.bind(this.controller))
+        router.post('/', this.jwtCheck, this.controller.insertOne.bind(this.controller))
         router.get('/:listingId', this.controller.getOne.bind(this.controller))
-        router.put('/:listingId', this.controller.buyItem.bind(this.controller))
+        router.put('/:listingId', this.jwtCheck,this.controller.buyItem.bind(this.controller))
         return router
     }
 }
