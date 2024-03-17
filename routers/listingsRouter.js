602d92ae3b07285d73b150437e77861b6e6847dc
patrickkok <pkok1996@gmail.com>
2024-03-17 18:42:00 +0800

    Install auth0, protect buy and create listing end points

--- routers/listingsRouter.js
+++ routers/listingsRouter.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+const router = express.Router();
 
 class ListingsRouter {
-    constructor(controller){
-        this.controller = controller
-    }
-    routes(){
-        // we will insert routes into here later on
-        router.get('/', this.controller.getAll.bind(this.controller))
-        router.post('/', this.controller.insertOne.bind(this.controller))
-        router.get('/:listingId', this.controller.getOne.bind(this.controller))
-        router.put('/:listingId', this.controller.buyItem.bind(this.controller))
-        return router
-    }
+  constructor(controller) {
+    this.controller = controller;
+  }
+  checkJWT = auth({
+    audience: process.env.AUDIENCE,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    tokenSigningAlg: "RS256",
+  });
+  routes() {
+    // we will insert routes into here later on
+    router.get("/", this.controller.getAll.bind(this.controller));
+    router.post(
+      "/",
+      this.checkJWT,
+      this.controller.insertOne.bind(this.controller)
+    );
+    router.get("/:listingId", this.controller.getOne.bind(this.controller));
+    router.put(
+      "/:listingId",
+      this.checkJWT,
+      this.controller.buyItem.bind(this.controller)
+    );
+    return router;
+  }
 }
 
-module.exports = ListingsRouter
+module.exports = ListingsRouter;
