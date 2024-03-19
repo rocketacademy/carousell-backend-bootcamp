fa10fed861b753d3d498af8f25a35e86b0f9132d
magiicloud <hongyun_rc@outlook.com>
2024-03-19 21:09:51 +0800

    Final version

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-const cors = require('cors')
-const express = require('express')
-require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+require("dotenv").config();
 
 // importing Routers
-const ListingsRouter = require('./routers/listingsRouter')
+const ListingsRouter = require("./routers/listingsRouter");
 
 // importing Controllers
-const ListingsController = require('./controllers/listingsController')
+const ListingsController = require("./controllers/listingsController");
 
 // importing DB
-const db = require('./db/models/index')
+const db = require("./db/models/index");
 const { listing, user } = db;
 
 // initializing Controllers -> note the lowercase for the first word
-const listingsController = new ListingsController(listing, user)
+const listingsController = new ListingsController(listing, user);
 
 // inittializing Routers
-const listingsRouter = new ListingsRouter(listingsController).routes()
+const listingsRouter = new ListingsRouter(listingsController).routes();
 
 const PORT = process.env.PORT;
 const app = express();
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: "https://carousell/api",
+  issuerBaseURL: `https://dev-sz0chkcuth5rdizf.us.auth0.com/`,
+});
+
+// This route doesn't need authentication
+app.get("/api/public", function (req, res) {
+  res.json({
+    message:
+      "Hello from a public endpoint! You don't need to be authenticated to see this.",
+  });
+});
+
+// This route needs authentication
+app.get("/api/private", checkJwt, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this.",
+  });
+});
 
 // Enable CORS access to this server
 app.use(cors());
@@ -28,7 +52,7 @@ app.use(cors());
 app.use(express.json());
 
 // enable and use router
-app.use('/listings', listingsRouter)
+app.use("/listings", listingsRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
